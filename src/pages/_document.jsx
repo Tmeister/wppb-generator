7d6a1c3143cbb1bae150b3f9fc930acdccbffd91
Tmeister <noone@tmeister.net>
2022-09-29 21:18:48 -0500

    Add GA

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function Document(props) {
   let pageProps = props.__NEXT_DATA__?.props?.pageProps
@@ -12,6 +13,7 @@ export default function Document(props) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
         />
+        <GoogleAnalytics trackPageViews />
       </Head>
       <body className="flex h-full flex-col">
         <Main />
