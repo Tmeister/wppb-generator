f9eca5bbaf8678f094027cf3b81bc2599f4fe4a3
Tmeister <noone@tmeister.net>
2023-04-17 09:52:47 -0600

    Change the OG image preview

--- src/pages/frost/index.jsx
+++ src/pages/frost/index.jsx
@@ -19,7 +19,7 @@ export default function Frost() {
           name="robots"
           content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"
         />
-        <link rel="canonical" href="https://wppb.me/Frost" />
+        <link rel="canonical" href="https://wppb.me/frost" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Frost Started Theme Generator" />
@@ -27,13 +27,13 @@ export default function Frost() {
           property="og:description"
           content="Frost starter theme generator, Stop wasting time searching and replacing text strings manually"
         />
-        <meta property="og:url" content="https://wppb.me/" />
+        <meta property="og:url" content="https://wppb.me/frost" />
         <meta property="og:site_name" content="Frost Started Theme Generator" />
         <meta property="og:updated_time" content="2020-03-27T18:28:44-06:00" />
-        <meta property="og:image" content="https://mugshotbot.com/m/mlqFqFjl" />
+        <meta property="og:image" content="https://mugshotbot.com/m/nQfbypj2" />
         <meta
           property="og:image:secure_url"
-          content="https://mugshotbot.com/m/mlqFqFjl"
+          content="https://mugshotbot.com/m/nQfbypj2"
         />
         <meta property="og:image:alt" content="Frost Started Theme Generator" />
         <meta name="twitter:card" content="summary_large_image" />
@@ -44,7 +44,7 @@ export default function Frost() {
         />
         <meta
           name="twitter:image"
-          content="https://mugshotbot.com/m/mlqFqFjl"
+          content="https://mugshotbot.com/m/nQfbypj2"
         />
       </Head>
       <Header />
